40da7c37e015705557c4c6c080da879a728ba3b0
Phillip Schichtel <phillip.public@schich.tel>
2016-01-13 02:35:49 +0100

    micro optimization

--- src/Day10.scala
+++ src/Day10.scala
@@ -8,20 +8,23 @@ object Day10 extends AoCApp {
     val input = "1113122113"
     val iterations = (40, 50)
 
+    @inline
+    private def toC(i: Int) = ('0' + i).toChar
+    @inline
+    private def toS(i: Int) = toC(i).toString
+
     @tailrec
     private def lookAndSay(s: String, offset: Int = 1, seqLength: Int = 1, said: Queue[String] = Queue()): String = {
-        if (offset >= s.length) said.mkString + seqLength + s.last
+        if (offset >= s.length) said.mkString + toC(seqLength) + s.last
         else {
             val a = s(offset - 1)
             val b = s(offset)
             if (a == b) lookAndSay(s, offset + 1, seqLength + 1, said)
-            else lookAndSay(s, offset + 1, 1, said :+ (seqLength.toString + a))
+            else lookAndSay(s, offset + 1, 1, said :+ (toS(seqLength) + a))
         }
     }
 
-    def transformer(input: String, it: Int) = {
-        lookAndSay(input)
-    }
+    def transformer(input: String, it: Int) = lookAndSay(input)
 
     // warmup
     //(0 until iterations._2).foldLeft(input)(transformer)
@@ -29,6 +32,6 @@ object Day10 extends AoCApp {
     val transformed = (0 until iterations._1).foldLeft(input)(transformer)
     println(s"Part 1: ${transformed.length}")
 
-    val transformedMore = (0 until iterations._2).foldLeft(transformed)(transformer)
+    val transformedMore = (iterations._1 until iterations._2).foldLeft(transformed)(transformer)
     println(s"Part 2: ${transformedMore.length}")
 }
