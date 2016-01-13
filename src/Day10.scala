abaee43d090ec6d586ba07c66cbe558b7569c36d
Phillip Schichtel <pschichtel@users.noreply.github.com>
2016-01-13 13:05:34 +0100

    even faster day10

--- src/Day10.scala
+++ src/Day10.scala
@@ -1,37 +1,38 @@
-import scala.collection.immutable.Queue
-import scala.math._
 import scala.annotation.tailrec
 
 object Day10 extends AoCApp {
     println("Day 10")
 
-    val input = "1113122113"
+    val input: VString = vstr("1113122113")
     val iterations = (40, 50)
 
     @inline
     private def toC(i: Int) = ('0' + i).toChar
+
+    type VString = Vector[Char]
     @inline
-    private def toS(i: Int) = toC(i).toString
+    private def vstr(s: String): VString = Vector(s:_*)
 
     @tailrec
-    private def lookAndSay(s: String, offset: Int = 1, seqLength: Int = 1, said: Queue[String] = Queue()): String = {
-        if (offset >= s.length) said.mkString + toC(seqLength) + s.last
+    private def lookAndSay(s: VString, offset: Int = 1, seqLength: Int = 1, said: VString = Vector.empty): VString = {
+        if (offset >= s.length) said :+ toC(seqLength) :+ s.last
         else {
             val a = s(offset - 1)
             val b = s(offset)
             if (a == b) lookAndSay(s, offset + 1, seqLength + 1, said)
-            else lookAndSay(s, offset + 1, 1, said :+ (toS(seqLength) + a))
+            else lookAndSay(s, offset + 1, 1, said :+ toC(seqLength) :+ a)
         }
     }
 
-    def transformer(input: String, it: Int) = lookAndSay(input)
+    def transformer(input: VString, it: Int) = lookAndSay(input)
 
     // warmup
-    //(0 until iterations._2).foldLeft(input)(transformer)
+    // (0 until iterations._2).foldLeft(input)(transformer)
 
+    val start = System.currentTimeMillis()
     val transformed = (0 until iterations._1).foldLeft(input)(transformer)
-    println(s"Part 1: ${transformed.length}")
+    println(s"Part 1: ${transformed.length} ${System.currentTimeMillis() - start}ms")
 
     val transformedMore = (iterations._1 until iterations._2).foldLeft(transformed)(transformer)
-    println(s"Part 2: ${transformedMore.length}")
+    println(s"Part 2: ${transformedMore.length} ${System.currentTimeMillis() - start}ms")
 }
