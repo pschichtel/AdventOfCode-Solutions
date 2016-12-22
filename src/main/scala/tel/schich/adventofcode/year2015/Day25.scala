074c52c7ca70ebd2eccab321ddb82d563d403015
Phillip Schichtel <phillip.public@schich.tel>
2016-12-23 00:58:42 +0100

    fast version of day 25 '15

--- src/main/scala/tel/schich/adventofcode/year2015/Day25.scala
+++ src/main/scala/tel/schich/adventofcode/year2015/Day25.scala
@@ -5,22 +5,30 @@ import java.util.concurrent.atomic.AtomicInteger
 import tel.schich.adventofcode.AoCApp
 import tel.schich.adventofcode.year2015.Day20.N
 
+import scala.annotation.tailrec
+
 object Day25 extends AoCApp {
 
-    def generateDiagonal(n: Int): Stream[(Int, Int)] = N.take(n).map(i => (i, n - (i - 1)))
-    def generateGrid: Stream[(Int, Int)] = N.flatMap(generateDiagonal)
+    @inline
+    def nextCode(currentCode: Long): Long = (currentCode * 252533) % 33554393
+
+    def findCode(startCode: Long, column: Int, row: Int): Long = {
+
+        @tailrec
+        def findNext(currentCode: Long, diagonal: Int, x: Int): Long = {
+            if (column == x && row == diagonal - (x - 1)) currentCode
+            else {
+                val next = nextCode(currentCode)
+                if (x == diagonal) findNext(next, diagonal + 1, 1)
+                else findNext(next, diagonal, x + 1)
+            }
+        }
+
+        findNext(startCode, 1, 1)
+    }
 
     val List(y, x) = "\\d+".r.findAllIn(inputText).toList
     val targetPos = (x.toInt, y.toInt)
 
-    val startCode = 20151125L
-    def nextCode(currentCode: Long): Long = (currentCode * 252533) % 33554393
-
-    val i = new AtomicInteger(1)
-    println(generateGrid.takeWhile(pos => pos != targetPos).foldLeft(startCode) { (current, pos) =>
-        val next = nextCode(current)
-        println(s"${i.getAndIncrement()} -> $pos: $current -> $next")
-        next
-    })
-
+    part(1, findCode(20151125L, x.toInt, y.toInt))
 }
