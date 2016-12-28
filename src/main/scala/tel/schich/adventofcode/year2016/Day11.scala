3dc4574cc6121fe44e7da56c30a26183ae25b5c3
Phillip Schichtel <phillip.public@schich.tel>
2016-12-28 03:21:38 +0100

    started with day 11 '16

---
+++ src/main/scala/tel/schich/adventofcode/year2016/Day11.scala
@@ -0,0 +1,23 @@
+package tel.schich.adventofcode.year2016
+
+import tel.schich.adventofcode.AoCApp
+
+object Day11 extends AoCApp {
+
+    var generator = "(\\w+) generator".r
+    var microchip = "([a-zA-Z]+)-compatible microchip".r
+
+    sealed trait TechObject {
+        def material: String
+    }
+    case class Generator(material: String) extends TechObject
+    case class Microchip(material: String) extends TechObject
+
+    val floors: Seq[Seq[TechObject]] = inputLines.map { s =>
+        generator.findAllMatchIn(s).map(m => Generator(m.group(1))).toList ++
+        microchip.findAllMatchIn(s).map(m => Microchip(m.group(1)))
+    }
+    val startFloor = 0
+
+    floors.foreach(println)
+}
