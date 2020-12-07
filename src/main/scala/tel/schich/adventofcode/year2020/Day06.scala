ac4e0ed9e7ab19582ed061963a492a92e98afb68
Phillip Schichtel <phillip@schich.tel>
2020-12-07 01:45:12 +0100

    day 6 using bit ops

--- src/main/scala/tel/schich/adventofcode/year2020/Day06.scala
+++ src/main/scala/tel/schich/adventofcode/year2020/Day06.scala
@@ -6,19 +6,21 @@ import tel.schich.adventofcode.shared.Parser._
 
 object Day06 extends AoCApp {
 
-    val parsePerson = parseAtLeastOnce(_.isLetter).map(_.toSet)
+    val parsePerson = parseAtLeastOnce(_.isLetter).map { person =>
+        person.toSeq.map(1 << ('a' - _)).foldLeft(0)(_ | _)
+    }
     val parseGroup = parseAllSeparated(parsePerson, parseLineBreak)
 
-    implicit val parseInput: Parser[Seq[Seq[Set[Char]]]] =
+    implicit val parseInput: Parser[Seq[Seq[Int]]] =
         parseAllSeparated(parseGroup, parseLineBreak.repeated(2))
 
     val groups = input(parseInput)
 
-    def solve[A](groups: Seq[Seq[Set[Char]]], f: (Set[Char], Set[Char]) => Set[Char]): Int = groups.map { group =>
-        group.reduce(f).size
+    def solve[A](groups: Seq[Seq[Int]], f: (Int, Int) => Int): Int = groups.map { group =>
+        java.lang.Integer.bitCount(group.reduce(f))
     }.sum
 
-    part(1, solve(groups, _ ++ _))
-    part(2, solve(groups, _ intersect _))
+    part(1, solve(groups, _ | _))
+    part(2, solve(groups, _ & _))
 
 }
