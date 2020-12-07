25b1a1a5cc83b36cf83d869ff1780fbe1a1dd05f
Phillip Schichtel <phillip@schich.tel>
2020-12-07 01:51:17 +0100

    minor cleanup

--- src/main/scala/tel/schich/adventofcode/year2020/Day06.scala
+++ src/main/scala/tel/schich/adventofcode/year2020/Day06.scala
@@ -1,8 +1,9 @@
 package tel.schich.adventofcode.year2020
 
 import tel.schich.adventofcode.shared.AoCApp
-import tel.schich.adventofcode.shared.Parser
 import tel.schich.adventofcode.shared.Parser._
+
+import java.lang.Integer.bitCount
 
 object Day06 extends AoCApp {
 
@@ -10,15 +11,12 @@ object Day06 extends AoCApp {
         person.toSeq.map(1 << ('a' - _)).foldLeft(0)(_ | _)
     }
     val parseGroup = parseAllSeparated(parsePerson, parseLineBreak)
-
-    implicit val parseInput: Parser[Seq[Seq[Int]]] =
-        parseAllSeparated(parseGroup, parseLineBreak.repeated(2))
+    val parseInput = parseAllSeparated(parseGroup, parseLineBreak.repeated(2))
 
     val groups = input(parseInput)
 
-    def solve[A](groups: Seq[Seq[Int]], f: (Int, Int) => Int): Int = groups.map { group =>
-        java.lang.Integer.bitCount(group.reduce(f))
-    }.sum
+    def solve(groups: Seq[Seq[Int]], f: (Int, Int) => Int) =
+        groups.map(group => bitCount(group.reduce(f))).sum
 
     part(1, solve(groups, _ | _))
     part(2, solve(groups, _ & _))
