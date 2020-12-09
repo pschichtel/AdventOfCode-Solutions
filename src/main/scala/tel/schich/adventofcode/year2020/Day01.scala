bbebfb9478fbab8155687a91ff7aba31412d4c39
Phillip Schichtel <phillip@schich.tel>
2020-12-10 00:09:54 +0100

    laziness makes a big difference

--- src/main/scala/tel/schich/adventofcode/year2020/Day01.scala
+++ src/main/scala/tel/schich/adventofcode/year2020/Day01.scala
@@ -3,23 +3,21 @@ package tel.schich.adventofcode.year2020
 import tel.schich.adventofcode.shared.AoCApp
 
 object Day01 extends AoCApp {
+    val values = asLines(Input2020.Day01).map(_.toInt).toList
 
-    val values = asLines(Input2020.Day01).map(_.toInt)
-
-    val part1Vales = for {
-        a <- values
-        b <- values
+    val part1Values = for {
+        a <- values.iterator
+        b <- values.iterator
         if a + b == 2020
     } yield a * b
+    part(1, part1Values.next())
 
-    part(1, part1Vales.head)
-
-    val part2Vales = for {
-        a <- values
-        b <- values
-        c <- values
+    val part2Values = for {
+        a <- values.iterator
+        b <- values.iterator
+        c <- values.iterator
         if a + b + c == 2020
     } yield a * b * c
 
-    part(2, part2Vales.head)
+    part(2, part2Values.next())
 }
