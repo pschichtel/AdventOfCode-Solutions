ca65a48e8b041c8d44f875605690d769f592b086
Phillip Schichtel <phillip@schich.tel>
2020-12-10 01:38:56 +0100

    a little cleaner

--- src/main/scala/tel/schich/adventofcode/year2020/Day09.scala
+++ src/main/scala/tel/schich/adventofcode/year2020/Day09.scala
@@ -24,13 +24,13 @@ object Day09 extends AoCApp {
         pair(offset, Nil)
     }
 
-    def findOutlier[T](cipherCode: ArraySeq[T], preambleSize: Int, op: (T, T) => T): Option[T] = {
+    def findOutlier[T](cipherCode: ArraySeq[T], preambleSize: Int)(implicit numeric: Numeric[T]): Option[T] = {
         @tailrec
         def find(i: Int): Option[T] = {
             if (i >= cipherCode.length) None
             else {
                 val n = cipherCode(i)
-                if (findPairs(cipherCode, i - preambleSize, preambleSize).exists(p => op(p._1, p._2) == n)) find(i + 1)
+                if (findPairs(cipherCode, i - preambleSize, preambleSize).exists(p => numeric.plus(p._1, p._2) == n)) find(i + 1)
                 else Some(n)
             }
         }
@@ -38,10 +38,9 @@ object Day09 extends AoCApp {
         find(preambleSize)
     }
 
-    def findWeakness(cipherCode: ArraySeq[Long], outlier: Long): Option[Long] = {
-
+    def findWeakness[T](cipherCode: ArraySeq[T], outlier: T)(implicit numeric: Numeric[T]): Option[T] = {
         @tailrec
-        def findWeakSequence(windowSize: Int): Option[Seq[Long]] = {
+        def findWeakSequence(windowSize: Int): Option[Seq[T]] = {
             if (windowSize > cipherCode.length) None
             else cipherCode.sliding(windowSize).find(_.sum == outlier) match {
                 case Some(value) => Some(value)
@@ -50,23 +49,17 @@ object Day09 extends AoCApp {
         }
 
         findWeakSequence(2) map { seq =>
-            seq.min + seq.max
+            numeric.plus(seq.min, seq.max)
         }
     }
 
     val preambleLength = 25
     val cipherCode = asLines(Input2020.Day09).map(_.toLong)
 
-    val outlier = findOutlier[Long](cipherCode, preambleLength, _ + _) match {
-        case Some(value) => value
-        case None => throw new Exception("no outlier found!")
-    }
+    val Some(outlier) = findOutlier(cipherCode, preambleLength)
 
     part(1, outlier)
 
-    val weakness = findWeakness(cipherCode, outlier) match {
-        case Some(value) => value
-        case None => throw new Exception("no weakness found!")
-    }
+    val Some(weakness) = findWeakness(cipherCode, outlier)
     part(2, weakness)
 }
